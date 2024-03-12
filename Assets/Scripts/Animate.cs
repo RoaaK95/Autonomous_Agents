55c711eab3ee290bf2b60e2172c91516538f269d
Roaa K <ruaakhuldoon.26@gmail.com>
2024-03-12 03:48:48 +0300

    Refactor
    -Assign each animal a class
    -Separate animate function from main class into a class of its own

---
+++ Assets/Scripts/Animate.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animate : MonoBehaviour
+{
+    [SerializeField] GameObject _FrontLegL, _FrontLegR, _RearLegL, _RearLegR;
+    private Vector3 legStartPosA = new Vector3(10.0f, 0f, 0f);
+    private Vector3 legEndPosA = new Vector3(-10.0f, 0f, 0f);
+    private Vector3 legStartPosB = new Vector3(-10.0f, 0f, 0f);
+    private Vector3 legEndPosB = new Vector3(10.0f, 0f, 0f);
+    float _LegsRotSpeed = 4.0f;
+    
+    public void WalkAnimation()
+    {
+        Quaternion legAngleFromA = Quaternion.Euler(legStartPosA);
+        Quaternion legAngleToA = Quaternion.Euler(legEndPosA);
+        Quaternion legAngleFromB = Quaternion.Euler(legStartPosB);
+        Quaternion legAngleToB = Quaternion.Euler(legEndPosB);
+        float lerp = 0.5f * (1.0f + Mathf.Sin(Mathf.PI * Time.realtimeSinceStartup * _LegsRotSpeed));
+        _FrontLegL.transform.localRotation = Quaternion.Lerp(legAngleFromA, legAngleToA, lerp);
+        _FrontLegR.transform.localRotation = Quaternion.Lerp(legAngleFromB, legAngleToB, lerp);
+        _RearLegL.transform.localRotation = Quaternion.Lerp(legAngleFromB, legAngleToB, lerp);
+        _RearLegR.transform.localRotation = Quaternion.Lerp(legAngleFromA, legAngleToA, lerp);
+    }
+
+}
