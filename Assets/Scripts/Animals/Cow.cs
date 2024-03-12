96156c2780f097a3b26f0bb100c2765ba8cf190f
Roaa K <ruaakhuldoon.26@gmail.com>
2024-03-12 22:38:10 +0300

    Add wander behaviour to cow

--- Assets/Scripts/Animals/Cow.cs
+++ Assets/Scripts/Animals/Cow.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Unity.AI;
+using UnityEngine.AI;
 public class Cow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private NavMeshAgent _agent;
+    private Animate _animate;
 
-    // Update is called once per frame
+
+    void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _animate = GetComponent<Animate>();
+    }
+    
+    void Seek(Vector3 location)
+    {
+        _agent.stoppingDistance = 2.0f;
+        _agent.SetDestination(location);
+        _animate.WalkAnimation();
+    }
+    Vector3 _wanderTarget = Vector3.zero;
+    void Wander()
+    {
+        float wanderRadius = 10.0f;
+        float wanderDistance = 20.0f;
+        float wanderJitter = 1.0f;
+
+        _wanderTarget += new Vector3(Random.Range(-1.0f, 1.0f) * wanderJitter, 0, Random.Range(-1.0f, 1.0f) * wanderJitter);
+        _wanderTarget.Normalize();
+        _wanderTarget *= wanderRadius;
+
+        Vector3 localTarget = _wanderTarget + new Vector3(0, 0, wanderDistance);
+        Vector3 worldTarget = gameObject.transform.InverseTransformVector(localTarget);
+        Seek(worldTarget);
+
+    }
     void Update()
     {
+        //Wander();
         
     }
+    
 }
