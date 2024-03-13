acba79e17e83e4743ff86d6073f558a4eb7c48db
Roaa K <ruaakhuldoon.26@gmail.com>
2024-03-13 03:57:36 +0300

    Apply cool down for the two behaviours

--- Assets/Scripts/Animals/Cow.cs
+++ Assets/Scripts/Animals/Cow.cs
@@ -1,36 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.AI;
 using UnityEngine.AI;
+using static UnityEngine.GraphicsBuffer;
 public class Cow : MonoBehaviour
 {
     private NavMeshAgent _agent;
     private Animate _animate;
     [SerializeField] private GameObject _waterTrough;
+    private int _invokeX = 2;
 
     void Awake()
     {
         _agent = GetComponent<NavMeshAgent>();
         _animate = GetComponent<Animate>();
     }
-    private void Start()
-    {
-        GoToWaterTrough();
-    }
+
     void Seek(Vector3 location)
     {
-        _agent.stoppingDistance = 2.0f;
-        _animate.WalkAnimation();
+        _agent.stoppingDistance = 1.0f;
+        _animate._isWalking = true;
+        _agent.isStopped = false;
         _agent.SetDestination(location);
-
     }
+
     Vector3 _wanderTarget = Vector3.zero;
     private void Wander()
     {
-        float wanderRadius = 10.0f;
-        float wanderDistance = 20.0f;
-        float wanderJitter = 1.0f;
+        float wanderRadius = 6.0f;
+        float wanderDistance = 12.0f;
+        float wanderJitter = 5.0f;
 
         _wanderTarget += new Vector3(Random.Range(-1.0f, 1.0f) * wanderJitter, 0, Random.Range(-1.0f, 1.0f) * wanderJitter);
         _wanderTarget.Normalize();
@@ -45,17 +44,50 @@ public class Cow : MonoBehaviour
     private void GoToWaterTrough()
     {
         Seek(_waterTrough.transform.position);
-        if (Vector3.Distance(transform.position, _waterTrough.transform.position) < 2f)
+        if (Vector3.Distance(_waterTrough.transform.position, transform.position) <= 1f)
         {
-            _agent.isStopped = true;
+            _animate._isWalking = false;
         }
-    }
 
- 
+    }
+    private bool TargetInRange(GameObject target)
+    {
+        if (Vector3.Distance(transform.position, target.transform.position) < 10)
+        {
+            return true;
+        }
+        return false;
+    }
+    bool coolDown = false;
+    private void BehaviourCoolDown()
+    {
+        coolDown = false;
+    }
     void Update()
     {
-        //Wander();
-        // GoToWaterTrough();
+        if (!coolDown)
+        {
+            if (TargetInRange(_waterTrough) && Random.Range(1, 10) <= _invokeX)
+            {
+                GoToWaterTrough();
+                coolDown = true;
+                Debug.Log("go to water invoked");
+                Invoke("BehaviourCoolDown", 10);
+
+            }
+            else
+            {
+                Wander();
+                coolDown = true;
+                Debug.Log("Wander invoked");
+                Invoke("BehaviourCoolDown", 15);
+
+            }
+
+        }
+
+        _animate.WalkAnimation();
+
     }
 
 }
