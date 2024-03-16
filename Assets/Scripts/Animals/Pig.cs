ced44aa1d92c80f3558cfc3f0f4a993dbf7f5faf
Roaa K <ruaakhuldoon.26@gmail.com>
2024-03-16 07:07:07 +0300

    Setup The Pig Script

--- Assets/Scripts/Animals/Pig.cs
+++ Assets/Scripts/Animals/Pig.cs
@@ -1,16 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class Pig : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private NavMeshAgent _agent;
+    private Animate _animate;
+    private float _stoppingDistance;
+    [SerializeField] private GameObject _player;
+    void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _animate = GetComponent<Animate>();
+    }
     void Start()
     {
-        
+        _agent.speed = 2.6f;
+        _stoppingDistance = 3.0f;
     }
 
-    // Update is called once per frame
+    private bool TargetInRange(GameObject target)
+    {
+        if (Vector3.Distance(transform.position, target.transform.position) < 10)
+        {
+            return true;
+        }
+        return false;
+    }
+    bool coolDown = false;
+    private void BehaviourCoolDown()
+    {
+        coolDown = false;
+    }
+
+    private void Animation(GameObject target)
+    {
+        if (Vector3.Distance(target.transform.position, transform.position) <= _stoppingDistance)
+        {
+            _animate._isWalking = false;
+        }
+        else
+        {
+            _animate._isWalking = true;
+        }
+        _animate.WalkAnimation();
+    }
     void Update()
     {
         
