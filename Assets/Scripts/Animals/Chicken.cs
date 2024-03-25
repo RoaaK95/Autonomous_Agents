b8261bbc0e8716e914fe6a62cd1563e2eda0cc3d
Roaa K <ruaakhuldoon.26@gmail.com>
2024-03-25 15:32:46 +0300

    Initialize Chicken Script

--- Assets/Scripts/Animals/Chicken.cs
+++ Assets/Scripts/Animals/Chicken.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Chicken : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private NavMeshAgent _agent;
+    private Animate _animate;
+    private float _playerSpeed;
+    private float _stoppingDistance;
+    [SerializeField] private GameObject _player;
+    private int _invokeX = 2;
+
+    void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _animate = GetComponent<Animate>();
+    }
+
     void Start()
     {
-        
+        _playerSpeed = Player.Speed;
+        _stoppingDistance = 3.0f;
+        _agent.speed = 2.0f;
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
